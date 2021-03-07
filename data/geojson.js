922d178665b52e583cc1f316e0da1fe0107d1121
Kat Sauma <katsauma@gmail.com>
2021-03-06 17:02:31 -0800

    endpoints for location, weather, reviews made with APIs

---
+++ data/geojson.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class geojson extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
---
+++ data/weatherjson.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class weatherjson extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
